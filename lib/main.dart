76b37e75f239f11b409b45da6b2efa1244d8b448
giselleShyp <alzawahragiselle@yahoo.com>
2026-04-10 13:55:14 +0300

    "Config ios"

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_portfolio/core/providers/theme_provider.dart';
 import 'package:my_portfolio/core/route/app_router.dart';
 import 'package:my_portfolio/core/theme/app_theme.dart';
+import 'package:my_portfolio/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(ProviderScope(child: MyApp()));
 }
